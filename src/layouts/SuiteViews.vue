ce8c8862e3b141c74ec133672621935c7d631245
Sadie Bartholomew <sadie.bartholomew@metoffice.gov.uk>
2019-07-02 17:05:44 +0100

    In progress on responsive panels

--- src/layouts/SuiteViews.vue
+++ src/layouts/SuiteViews.vue
@@ -10,6 +10,18 @@
         <v-fade-transition mode="out-in">
           <slot/>
         </v-fade-transition>
+
+  <auto-responsive
+    v-bind="options"
+  >
+        <material-card v-for="item in data" :style="style" class="item" :key="item"
+            text="Suite"
+            title={{ item }}
+            color="green"
+        >
+        </material-card>
+  </auto-responsive>
+
       </div>
       <Footer/>
     </v-content>
@@ -18,9 +30,13 @@
 
 <script>
 import Toolbar from '@/components/core/Toolbar'
-import Alert from "@/components/core/Alert"
+import Alert from '@/components/core/Alert'
 import Drawer from '@/components/core/Drawer'
 import Footer from '@/components/core/Footer'
+import AutoResponsive from 'autoresponsive-vue'
+
+import Vue from 'vue'
+Vue.use(AutoResponsive)
 
 export default {
   name: 'SuiteViews',
--- src/views/SuiteViews.vue
+++ src/views/SuiteViews.vue
@@ -11,8 +11,18 @@
 export default {
   metaInfo () {
     return {
-      title: 'Cylc UI | Dashboard'
+      title: 'Cylc UI | Suite Views'
     }
-  }
+  },
+  data() {
+    return {
+      data: ["my_suite_1_view_1", "my_suite_2_view_1", "my_suite_3_view_1",
+             "my_suite_1_view_2", "my_suite_2_view_2", "my_suite_3_view_2"],
+      options: {
+containerWidth: 10,
+containerHeight: 10,
+      }
+    }
+  },
 }
 </script>
